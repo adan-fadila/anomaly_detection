405940b2247ca076546b8f0885e700f7136a5444
adan fadila <adanfadila4@gmail.com>
2025-04-07 14:22:21 +0300

    send data to node

--- utils/node_communicator.py
+++ utils/node_communicator.py
@@ -1,6 +1,6 @@
 import requests
 from .logger import logger
-
+import json
 class NodeCommunicator:
     def __init__(self, base_url="http://127.0.0.1:8001"):
         self.base_url = base_url
@@ -13,7 +13,7 @@ class NodeCommunicator:
     def send_to_node(self, endpoint_key, data):
         try:
             api_url = f"{self.base_url}{self.endpoints[endpoint_key]}"
-            response = requests.post(api_url, json=data)
+            response = requests.post(api_url, json=json.dumps(data))
             logger.info(f"Sent data to {api_url}, Node.js response status: {response.status_code}")
             return response
         except Exception as e:
